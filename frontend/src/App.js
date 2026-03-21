2e49aae31b50e3303aeb04ebfaf6af6d92b5fbc6
ISABELLA_HE <HE.I@northeastern.edu>
2026-03-21 12:17:53 -0400

    Removed default react stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,19 +8,6 @@ function App() {
       <nav classname = "navbar">
         <span className="navbar-logo">FarmerHealth</span>
       </nav>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
