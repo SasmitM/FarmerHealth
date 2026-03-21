40c44a42e344998c6fb0111e8c6541abef50dfdb
ISABELLA_HE <HE.I@northeastern.edu>
2026-03-21 12:06:02 -0400

    testing navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
+      <nav classname = "navbar">
+        <span className="navbar-logo">FarmerHealth</span>
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
